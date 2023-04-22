963e3ddfa72027cad930a9df6a41352ff0d7ff96
Racheljjn <jian1160@mylaurier.ca>
2023-04-21 20:24:55 -0400

    persist user login

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,60 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 
-const Home = () => {
+const Home = ({ loggedIn, setLoggedIn }) => {
 
-  const userLogin = async (e) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let loggedIn = localStorage.getItem('loggedIn');
+
+    if(loggedIn) {
+      setLoggedIn(true);
+      setLoading(false);
+    }
+  }, []);
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    await fetch("https://127.0.0.1:5000/api/discordLogin")
+    setLoading(true)
+
+    await fetch("https://ec2-44-212-203-117.compute-1.amazonaws.com:5000/api/discordLogin")
       .catch(console.error)
       .then(response => response.json())
-      .then(data => {
-        console.log(data)
+      .then((data) => {
+        localStorage.setItem('loggedIn', true);
+        setLoggedIn(true);
+
         window.location.href = data.auth_url;
-      })
-  }
+      });
+  };
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    await fetch("https://ec2-44-212-203-117.compute-1.amazonaws.com:5000/api/discordLogout")
+      .catch(console.error)
+      .then(response => {
+        console.log(response)
+        localStorage.removeItem('loggedIn');
+        setLoggedIn(false);
+      });
+  };
 
   return (
     <div className="layout">
-      <div className="login_page">
-        <button onClick={userLogin}>login with discord</button>
+      <div className="loading">
+        {loading ? (
+        <p>Loading...</p>
+        ) : loggedIn ? (
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+        ) : (
+        <div>
+          <button onClick={handleLogin}>Login with discord</button>
+        </div>
+        )}
       </div>
+      
     </div>
   );
 }

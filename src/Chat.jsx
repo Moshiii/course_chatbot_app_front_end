bf51632734e0857ec22382159d64c6edbf636b9b
Moshi Wei <wmswms938@gmail.com>
2023-05-03 20:30:31 -0400

    update chat with buttons

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,35 +6,34 @@ import Cookies from 'universal-cookie';
 
 // const base_url = process.env.REACT_APP_AWS;
 
-const Chat =({ loggedIn, setLoggedIn }) => {
+const Chat = ({ loggedIn, setLoggedIn }) => {
 
-  const welcomeMsg = [
+  const [isTyping, setIsTyping] = useState(false);
+  const [messages, setMessages] = useState([
     {
-      message: "Hello, I'm your professor",     
+      message: "Hello, I'm your professor",
       sentTime: "just now",
       sender: "ChatGPT"
     }
-  ];
-  const [isTyping, setIsTyping] = useState(false);
-  const [messages, setMessages] = useState(welcomeMsg);
+  ]);
 
   const cookies = new Cookies();
 
   useEffect(() => {
 
+    let allMessages = cookies.get('allMessages');
+
+    allMessages && setMessages(allMessages);
+
     let loggedIn = localStorage.getItem('loggedIn');
 
-    if(loggedIn) {     
-      let allMessages = cookies.get('allMessages');
-      if(allMessages) {
-        setMessages(allMessages);
-      }
-
+    if (loggedIn) {
       setLoggedIn(true);
-    } 
-  },[]);
+    }
+  }, []);
 
   const handleSend = async (message) => {
+    console.log(message)
 
     const newMessage = {
       message,
@@ -43,7 +42,7 @@ const Chat =({ loggedIn, setLoggedIn }) => {
     };
 
     const allMessages = [...messages, newMessage]; // old msgs + new
-    
+
     setMessages(allMessages);
     setIsTyping(true);
     cookies.set('allMessages', allMessages, { path: '/chat' });
@@ -51,9 +50,9 @@ const Chat =({ loggedIn, setLoggedIn }) => {
   };
 
   const processMessageToChatGPT = async (chatMessages) => {
-    
+
     const token = cookies.get('access_token');
-  
+
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -61,27 +60,29 @@ const Chat =({ loggedIn, setLoggedIn }) => {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message}
+      return { role: role, content: messageObject.message }
     });
 
     const apiRequestBody = [...apiMessages];
+    console.log(apiRequestBody);
 
-    await fetch("https://ec2-44-212-203-117.compute-1.amazonaws.com:5000/api/openai", 
-    {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${token}`,       
-      },
-      body: JSON.stringify(apiRequestBody)})
-        .catch(console.error)
-        .then(response => (response.json()))
-        .then((data) => {
-          setMessages([...chatMessages, {
-            message: data[data.length - 1].content,
-            sender: "ChatGPT"
-          }]);
-          setIsTyping(false);
-    });
+    await fetch("https://ec2-44-212-203-117.compute-1.amazonaws.com:5000/api/openai",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify(apiRequestBody)
+      }).then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        setMessages([...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]);
+        setIsTyping(false);
+      });
   };
 
   return (
@@ -89,19 +90,35 @@ const Chat =({ loggedIn, setLoggedIn }) => {
       <div className="layout">
         {
           loggedIn ? <MainContainer>
-            <ChatContainer>                
-              <MessageList 
-                scrollBehavior="smooth" 
-                typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
-              > 
+            <ChatContainer>
+              <MessageList
+                scrollBehavior="smooth"
+                typingIndicator={isTyping ? <TypingIndicator content="Virtual Professor is typing" /> : null}
+              >
                 {messages.map((message, i) => {
                   return <Message key={i} model={message} />
                 })}
               </MessageList>
-              <MessageInput placeholder="Type message here" onSend={handleSend} />        
+              {/* <MessageInput placeholder="Type message here" onSend={handleSend} />         */}
+              <MessageInput
+                placeholder="Type message here"
+                onSend={handleSend}
+                buttons={[
+                  {
+                    key: 'button1',
+                    icon: <YourFirstButtonIcon />,
+                    onClick: () => console.log('First button clicked'),
+                  },
+                  {
+                    key: 'button2',
+                    icon: <YourSecondButtonIcon />,
+                    onClick: () => console.log('Second button clicked'),
+                  }
+                ]}
+              />
             </ChatContainer>
-          </MainContainer> 
-          : <div>please log in first</div>
+          </MainContainer>
+            : <div>please log in first</div>
         }
       </div>
     </div>
